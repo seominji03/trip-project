b636bc24feb690f80a04fcec7329c6189207278a
hyojin0326 <gywlsl0826@naver.com>
2023-02-06 19:43:39 +0900

    add html 생성과 연결

--- trip/src/main/java/one/trip/controller/MemberController.java
+++ trip/src/main/java/one/trip/controller/MemberController.java
@@ -3,6 +3,7 @@ package one.trip.controller;
 import one.trip.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -11,5 +12,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping(value = "/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
     }
 }
