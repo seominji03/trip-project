3dbeb59c40af4f75aa6d81575a9f65121d8b711d
hyojin0326 <gywlsl0826@naver.com>
2023-02-06 18:54:15 +0900

    add 회원 컨트롤러->서비스->리포지토리 DI?

--- trip/src/main/java/one/trip/repository/MemoryMemberRepository.java
+++ trip/src/main/java/one/trip/repository/MemoryMemberRepository.java
@@ -1,9 +1,10 @@
 package one.trip.repository;
 
 import one.trip.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
-
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
